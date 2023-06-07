da71c5c6219bfa8af445c1418b0e1eb376121c28
minatubouti <135243296>
2023-06-07 11:26:47 +0000

    chapter7

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  get '/top' => 'homes#top'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+  
+  get 'top' => 'homes#top'
+  resources :lists
+  
 end
